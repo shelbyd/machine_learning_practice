53cbe5d4b82ef49a1d8b2f8f61b74bff97face9e
Shelby Doolittle <shelby@shelbyd.com>
2017-04-01 16:47:25 -0400

    make CNN classifier exportable

--- mnist/cnn.py
+++ mnist/cnn.py
@@ -2,67 +2,68 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten, ZeroPadding2D
-from keras.optimizers import RMSprop
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-def normalize_image(image):
-  items, height, width = image.shape
-  if keras.backend.image_data_format() == 'channels_first':
-    image = image.reshape(items, 1, height, width)
-  else:
-    image = image.reshape(items, height, width, 1)
-  return image.astype('float32') / 255
-
-x_train = normalize_image(x_train)
-x_test = normalize_image(x_test)
-
-y_train = keras.utils.to_categorical(y_train)
-y_test = keras.utils.to_categorical(y_test)
-
-if keras.backend.image_data_format() == 'channels_first':
-  input_shape = (1, x_train.shape[1], x_train.shape[2])
-else:
-  input_shape = (x_train.shape[1], x_train.shape[2], 1)
 
 try:
-  model = keras.models.load_model('models/mnist/cnn.h5')
+  classifier = keras.models.load_model('models/mnist/cnn.h5')
 except:
-  model = Sequential()
+  classifier = Sequential()
 
-  model.add(ZeroPadding2D(input_shape=input_shape))
-  model.add(Conv2D(32, (3, 3)))
-  model.add(Activation('relu'))
+  classifier.add(ZeroPadding2D(input_shape=input_shape))
+  classifier.add(Conv2D(32, (3, 3)))
+  classifier.add(Activation('relu'))
 
-  model.add(ZeroPadding2D())
-  model.add(Conv2D(64, (3, 3)))
-  model.add(Activation('relu'))
+  classifier.add(ZeroPadding2D())
+  classifier.add(Conv2D(64, (3, 3)))
+  classifier.add(Activation('relu'))
 
-  model.add(Conv2D(64, (2, 2), strides=(2, 2)))
-  model.add(Activation('relu'))
+  classifier.add(Conv2D(64, (2, 2), strides=(2, 2)))
+  classifier.add(Activation('relu'))
 
-  model.add(Dropout(0.2))
-  model.add(Flatten())
+  classifier.add(Dropout(0.2))
+  classifier.add(Flatten())
 
-  model.add(Dense(128))
-  model.add(Activation('relu'))
-  model.add(Dropout(0.5))
+  classifier.add(Dense(128))
+  classifier.add(Activation('relu'))
+  classifier.add(Dropout(0.5))
 
-  model.add(Dense(y_train.shape[1]))
-  model.add(Activation('softmax'))
+  classifier.add(Dense(y_train.shape[1]))
+  classifier.add(Activation('softmax'))
 
-model.summary()
+if __name__ == "__main__":
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model.compile(loss='categorical_crossentropy',
-        optimizer=keras.optimizers.Adadelta(),
-        metrics=['accuracy'])
+  def normalize_image(image):
+    items, height, width = image.shape
+    if keras.backend.image_data_format() == 'channels_first':
+      image = image.reshape(items, 1, height, width)
+    else:
+      image = image.reshape(items, height, width, 1)
+    return image.astype('float32') / 255
 
-history = model.fit(x_train, y_train,
-        batch_size=128,
-        verbose=1,
-        validation_data=(x_test, y_test),
-        callbacks=[keras.callbacks.ModelCheckpoint('models/mnist/cnn.h5', verbose=1)])
+  x_train = normalize_image(x_train)
+  x_test = normalize_image(x_test)
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+  y_train = keras.utils.to_categorical(y_train)
+  y_test = keras.utils.to_categorical(y_test)
+
+  if keras.backend.image_data_format() == 'channels_first':
+    input_shape = (1, x_train.shape[1], x_train.shape[2])
+  else:
+    input_shape = (x_train.shape[1], x_train.shape[2], 1)
+
+
+  classifier.summary()
+
+  classifier.compile(loss='categorical_crossentropy',
+          optimizer=keras.optimizers.Adadelta(),
+          metrics=['accuracy'])
+
+  history = classifier.fit(x_train, y_train,
+          batch_size=128,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          callbacks=[keras.callbacks.ModelCheckpoint('models/mnist/cnn.h5', verbose=1)])
+
+  score = classifier.evaluate(x_test, y_test, verbose=0)
+  print('Test loss:', score[0])
+  print('Test accuracy:', score[1])

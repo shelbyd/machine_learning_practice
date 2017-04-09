f636c15cfc85e812f0f38c182950bb1ea595818b
Shelby Doolittle <shelby@shelbyd.com>
2017-04-09 19:02:25 -0400

    apparently 3.5 requires explicit encoding

--- mnist/generator/checkpointer.py
+++ mnist/generator/checkpointer.py
@@ -1,5 +1,5 @@
 import hashlib
 
 def generate_checkpoint_path(base, model):
-  model_hash = hashlib.sha1(model.to_json()).hexdigest()[0:8]
-  return "%s_%s.h5" % (base, model_hash)
+  model_hash = hashlib.sha1(model.to_json().encode('utf-8')).hexdigest()[0:8]
+  return "%s.h5" % (base)
